ef73d3ffae395801c80e90ef34ead5d6422fdf6c
Steffen Panning <steffen.panning@googlemail.com>
2012-05-14 23:19:09 +0200

    untabify

--- rel/overlay/common/site/src/restful_form.erl
+++ rel/overlay/common/site/src/restful_form.erl
@@ -16,34 +16,34 @@ inner_body() ->
     case wf:q(restful_method) of
         "post"  -> [
             #h1{text="Restful form results"},
-	    #table{rows=[
-		#tablerow{cells=[
-		    #tablecell{text="text_input"},
-		    #tablecell{text=wf:q(text_input)}]},
-		#tablerow{cells=[
-		    #tablecell{text="radio_group"},
-		    #tablecell{text=wf:q(radio_group)}]}
-	    ]}
-	];
+            #table{rows=[
+                #tablerow{cells=[
+                    #tablecell{text="text_input"},
+                    #tablecell{text=wf:q(text_input)}]},
+                #tablerow{cells=[
+                    #tablecell{text="radio_group"},
+                    #tablecell{text=wf:q(radio_group)}]}
+            ]}
+        ];
         _else -> [
             #h1{text="Restful form"},
-	    #restful_form{id=restful_form, method=post, body=[
-		#table{rows=[
-		    #tablerow{cells=[
-			#tablecell{text="text_input"},
-			#tablecell{body=[#textbox{id=text_input}]}]},
-		    #tablerow{cells=[
-			#tablecell{text="radio_group"},
-			#tablecell{body=[
-			    #radio{html_name=radio_group,
-				text=true,
-				value=true,
-				checked=true},
-			    #radio{html_name=radio_group, 
-				text=false,
-				value=false, 
-				checked=false}]}
-		    ]}]},
+            #restful_form{id=restful_form, method=post, body=[
+                #table{rows=[
+                    #tablerow{cells=[
+                        #tablecell{text="text_input"},
+                        #tablecell{body=[#textbox{id=text_input}]}]},
+                    #tablerow{cells=[
+                        #tablecell{text="radio_group"},
+                        #tablecell{body=[
+                            #radio{html_name=radio_group,
+                                text=true,
+                                value=true,
+                                checked=true},
+                            #radio{html_name=radio_group, 
+                                text=false,
+                                value=false, 
+                                checked=false}]}
+                    ]}]},
                 #restful_submit{}]}
         ]
     end.
